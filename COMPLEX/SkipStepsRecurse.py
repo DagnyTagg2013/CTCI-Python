438489361d6ab47313fce117497acf8924903e74
DagnyTagg2013 <DagnyTagg2013@gmail.com>
2017-03-09 04:10:26 -0800

    cleanup SkipStepsRecurse but save old messy version to SkipStepsRecurseMessy.py

--- COMPLEX/SkipStepsRecurse.py
+++ COMPLEX/SkipStepsRecurse.py
@@ -1,6 +1,3 @@
-
-
-
 #!/bin/python
 
 import sys
@@ -10,7 +7,8 @@ import os
 # - n is MAX TOTAL number of moves
 # - @ each move can STAY at current step (start from 0) or hop (+i) where i is current move number
 # - returns MAX step reached across ALL possible move paths
-# - e.g. 2,2 =>
+# - CANNOT land on BLOCKED step given by K
+# - e.g. 2,2 => 3
 #   e.g. 3,3 => 5
 def  maxStep(n, k):
 
@@ -20,61 +18,6 @@ def  maxStep(n, k):
     (validPath, maxStepForPath) = maxStepRecurse(currMove, currStep, k, n)
 
     return maxStepForPath
-
-# *******************************************************************************************************
-# STAB2:  ITERATIVE DRIVER of OUTER-LOOP function
-# * DECISION TREE:  EACH move offers TWO choices;
-# - STAY
-# - or HOP up Num Steps EQUAL to current Move Number
-# * 2 EXP N FINAL path possibilities at LEAVes
-# - ELIMINATE intermediate paths via CHECK against blockedStep
-# * recursion can BLOW-UP the STACK on EXPONENTIAL complexity for LARGE N!
-#   SO INSTEAD use WHILE iterate through EACH CHOICE BRANCHING-POINT; LIMIT to maxNumMoves
-# - RETURN TUPLEs with UPDATED state for NEXT iteration
-# * DUNNO: look into reuse/memoization of CACHE-MAP of prior-calculated results to
-# BLOCK REDUNDANT PATHs on EXPONENTIAL CHOICE-TREE!
-# eg reuse of prior CUMULATIVE state (like SUM) from SUBSET of problem-space
-# * ISSUE:  LOCAL optima does NOT imply GLOBAL optima; i.e. for N=3; maxVal at i=1; 0 < 1
-#           BUT; maxVal at i=3; 5 > 4 (0, 2, 5; vs 1, 1, 4)
-# * SOLUTION:  - represent each possible path via 2expN Bit Array
-#              - accumulate CURRENT STEP; OR -1 for Bit Array; as progressing through MOVES from 1 to N
-#              - FINAL LOOP through ALL results to see MAXIMUM; since local MAXIMUM of subset does NOT determine OVERALL MAXIMUM!
-
-"""
-def iterPathsToMaxFinalStep(maxNumMoves, blockedStep):
-
-    # STEP 1:
-    # initialize STARTing States, and iteration vars
-    # THEN ID Iteration Variables
-    currStep = 0;  # POSITION is 0th step
-    currMove = 1;  # MOVE is DELTA from currStep to get to nextStep
-
-
-    # STEP2:  ACCUMULATE FINAL STEP POSITION RESULTs;
-    #         INDEXED-BY BIT-ARRAY of PATH-NAV-CHOICES; (0 to STAY, 1 to GO at position X)
-    #         STARTING index 1
-    #         - or 2 EXP N possible paths
-    cacheFinalStepByMovePath = {}
-
-    # DUNNO:  START from END; then step BACK; or START at START, go FORWARD?
-    while (currMove <= maxNumMoves):
-
-        # STEP3:  DECIDE on NEXT SKIP MOVE on PATH; then eliminate all BAD DOWNSTREAM DECISION PATHs!
-
-        # STEP 4: DECIDE REMAIN at current position, then eleminate all BAD DOWNSTREAM DECISION PATHs!
-        # TODO:  set all cacheFinalStepByMovePath elements to currStep
-        # - where bit is 1 for decision to SKIP steps at index currMove
-        # - where bit is 0 for decision to STAY at index currMove
-
-        # STEP5:  ITERATE control-variables where currStep ALREADY handled!
-        currMove = currMove + 1
-
-
-    # STEP 6:  TWO-PASS APPROACH to now scan ACROSS LOWEST-GRANULARITY-CUMULATIVE RESULTs;
-    #          to derive MAX-STEP! or 2 exp N LEAVEs of FINAL Decision Tree!
-
-}
-"""
 
 # *******************************************************************************************************
 # STAB1:  RECURSIVE FUNCTION
@@ -138,83 +81,3 @@ def main(args):
 # http://stackoverflow.com/questions/22492162/understanding-the-main-method-of-python
 if __name__ == '__main__':
     main(sys.argv)
-
-# *****************************************************************************
-# TODO:  read input from STDIN; then write result of trial to output file!
-
-"""
-def readInput():
-
-    print "Enter matrix dimensions!"
-
-    # ATTN:  cast to int!
-    maxRows, maxCols = map(int, sys.stdin.readline().split(','))
-
-    # ATTN THIS WON'T work for initial space allocation!
-
-    # matrix = []
-    # for i in range(0, maxRows):
-    #     matrix[i] = list(map(int, sys.stdin.readline().split(',')))
-
-    # allocate space here!
-    matrix = numpy.zeros((maxRows, maxCols))
-
-    print "Enter matrix data by CSV integer rows!!"
-    for i in range(0, maxRows):
-        row_data = list(map(int, sys.stdin.readline().split(',')))
-        for j in range(0, maxCols):
-            matrix[i][j] = row_data[j]
-
-    print matrix
-
-    return matrix
-
-"""
-
-# *******************************************************************************************************
-# TODO:  read input from STDIN; then write result of trial to output file FROM THEIR FRAMEWORK CODE!
-
-"""
-    # Complete the function below.
-
-    def  maxStep(n, k):
-
-    f = open(os.environ['OUTPUT_PATH'], 'w')
-
-
-    _n = int(raw_input());
-
-
-    _k = int(raw_input());
-
-    res = maxStep(_n, _k);
-    f.write(str(res) + "\n")
-
-    f.close()
-
-"""
-
-
-# *******************************************************************************************************
-# TODO:  following is Java 1.8 boilerplate!
-
-"""
-    public static void main(String[] args) throws IOException{
-        Scanner in = new Scanner(System.in);
-        final String fileName = System.getenv("OUTPUT_PATH");
-        BufferedWriter bw = new BufferedWriter(new FileWriter(fileName));
-        int res;
-        int _n;
-        _n = Integer.parseInt(in.nextLine().trim());
-
-        int _k;
-        _k = Integer.parseInt(in.nextLine().trim());
-
-        res = maxStep(_n, _k);
-        bw.write(String.valueOf(res));
-        bw.newLine();
-
-        bw.close();
-    }
-
-"""
