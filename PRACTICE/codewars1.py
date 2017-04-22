acaafec5b5db84c5be0c03eb5fdc96ebba89f51e
DagnyTagg2013 <DagnyTagg2013@gmail.com>
2017-04-22 07:03:04 -0700

    trying codewars exercises

---
+++ PRACTICE/codewars1.py
@@ -0,0 +1,23 @@
+__author__ = 'dagny t'
+
+"""
+  maybe javascript version issue on having to CAST to int!
+"""
+
+"""
+def multiply1(a, b):
+  return (a * b)
+
+def multiply2(a, b):
+  return (int(a) * int(b))
+
+print multiply1(2, 3)
+
+print multiply2(2, 3)
+
+"""
+
+def multiply(a, b):
+  return (a * b)
+
+print multiply(2,3)
